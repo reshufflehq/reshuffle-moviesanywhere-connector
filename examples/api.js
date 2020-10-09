80b67b1cd71c2b004e8a3512c5b2e8fb3ca2c8f2
Avner Braverman <avner@binaris.com>
2020-10-09 14:26:24 -0700

    Update examples

--- examples/api.js
+++ examples/api.js
@@ -9,12 +9,12 @@ const http = new HttpConnector(app)
 // record, try 10.5240/DF48-AB62-4486-C185-9E1B-4
 
 http.on({ method: 'GET', path: '/' }, async (event) => {
-  const eidr = event.context.req.query.eidr
+  const eidr = event.req.query.eidr
   if (!/^10\.5240\/([0-9A-F]{4}-){5}[0-9A-Z]$/.test(eidr)) {
-    return event.context.res.status(400).send(`Invalid EIDR ID: ${eidr}`)
+    return event.res.status(400).send(`Invalid EIDR ID: ${eidr}`)
   }
   const data = await ma.getTitleByEIDR(eidr)
-  return event.context.res.json(data)
+  return event.res.json(data)
 })
 
 app.start(8000)
